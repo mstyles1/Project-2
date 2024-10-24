92153713570b428ec844cda0a061ca8b87f1d088
matt styles <styles.matt135@gmail.com>
2024-10-24 14:00:53 -0400

    Api key first pasted

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+async function clickHandler_fetchdata(AYyh40hWbkpevIR5LRy7GIN3YSd382bx) {
+    let response = await fetch ()
+    let data = await response.json()
+    data.forEeach(element => {
+        console.log(element)
+    });
+}
